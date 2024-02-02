655a382ef0ebbe1d39ed01130842ee45363abe63
kamacoder <kamacoder.work@gmail.com>
2024-02-02 14:54:49 +0100

    update

--- index.php
+++ index.php
@@ -1,19 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
 </head>
 <body>
-    
-
-    <form action="post" id="myForm">
-        <input type="text">
-        <input type="text">
-        <input type="text" class="send_btn" value="Send Data">
-    </form>
 
 
+
+<script src="script.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
